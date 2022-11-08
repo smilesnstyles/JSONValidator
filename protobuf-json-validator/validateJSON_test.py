0609875e0be54e6d66fe9e9cca32fd0d2218051d
smilesnstyles <smilesnstyles@gmail.com>
2022-11-08 15:20:45 -0800

    Update validateJSON_test.py

--- protobuf-json-validator/validateJSON_test.py
+++ protobuf-json-validator/validateJSON_test.py
@@ -5,33 +5,28 @@ import os
 import MyProto_pb2
 import google.protobuf.json_format as jsonFormat
 
-argc = len(sys.argv)
-if argc >= 2 :
-    if sys.argv[1].endswith(".json") or sys.argv[1].endswith(".JSON") :
-        print("Received Input File: ",sys.argv[1]);
-        path = os.path.join(os.getcwd(),sys.argv[1]);
-        print("Path : ",os.path.exists(path))
-        if os.path.exists(path) :
-            jsonfile = open(path,"rb");
-            if jsonfile : 
-                print("File open Successfully");
-                jsonstring = jsonfile.read();
-                print(jsonstring)
-                someMessage = MyProto_pb2.SomeMessage();
-                try:
-                    jsonFormat.Parse(jsonstring,someMessage)
-                    print(someMessage.color);
-                    jsonfile.close();
-                except jsonFormat.ParseError:
-                    print("JSON Parse Error")
-                    jsonfile.close();
-            else :
-                print("File Open Failed");
-        else :
-            print("No File ", path," Exists");
-    else :
-        print("Input File Extention is not JSON");
+print("Starting JSON Validation in Python Script");
+path = os.path.join(os.getcwd(),"data-elements/MyJson.json");
+print("Path : ", path);
+if os.path.exists(path) :
+	jsonfile = open(path,"rb");
+	if jsonfile : 
+		print("File open Successfully");
+		jsonstring = jsonfile.read();
+		jsonfile.close();
+		if jsonstring :
+			print("JSON File Read Successfully'");
+			someMessage = MyProto_pb2.SomeMessage();
+			try:
+				jsonFormat.Parse(jsonstring,someMessage)
+				print(someMessage.color);
+				jsonfile.close();
+			except jsonFormat.ParseError:
+				print("JSON Parse Error")
+				jsonfile.close();
+		else :
+			print("Unable to Read JSON file to String")
+	else :
+		print("File Open Failed");
 else :
-    print("Invalid arguments");
-    print("Help : \n Syntax : python validateJSON.py <jsonFileName> "); 
-    
+    print("No File ", path," Exists");    
