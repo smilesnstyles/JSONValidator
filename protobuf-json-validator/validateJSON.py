ca9a6bd2b3b45eccbf317b49c0d126a8ba4ec198
smilesnstyles <smilesnstyles@gmail.com>
2022-11-11 01:33:57 -0800

    Update validateJSON.py

--- protobuf-json-validator/validateJSON.py
+++ protobuf-json-validator/validateJSON.py
@@ -5,23 +5,37 @@ import os
 import VIPData_pb2
 import google.protobuf.json_format as jsonFormat
 
-print("Starting JSON Validation in Python Script");
-path = os.path.join(os.getcwd(),"data-elements/EngineSpeed.json");
-print("Path : ", path);
-if os.path.exists(path) :
-	jsonfile = open(path,"rb");
-	if jsonfile : 
-		print("File open Successfully");
-		jsonstring = jsonfile.read();
-		jsonfile.close();
-		if jsonstring :
-			print("JSON File Read Successfully'");
-			vipdata = VIPData_pb2.VIPData();
-			jsonFormat.Parse(jsonstring,vipdata)
-			print("vipdata  speed max_val: ",vipdata.StatEngineSpeed.max_val);
-		else :
-			print("Unable to Read JSON file to String")
-	else :
-		print("File Open Failed");
+argc = len(sys.argv)
+if argc >= 2 :
+    if sys.argv[1].endswith(".json") or sys.argv[1].endswith(".JSON") :
+        print("Received Input File: ",sys.argv[1]);
+        path = os.path.join(os.getcwd(),sys.argv[1]);
+        print("Path : ",os.path.exists(path))
+        if os.path.exists(path) :
+            jsonfile = open(path,"rb");
+            if jsonfile : 
+                print("File open Successfully");
+                jsonstring = jsonfile.read();
+                #print(jsonstring)
+                VIPData = VIPData_pb2.VIPData()
+                jsonFormat.Parse(jsonstring,VIPData)
+                if VIPData :
+                    print("Engien Speed PGN ", VIPData.STAT_ENGINE_SPEED.J1939.attributes.PGN);
+                    print("Rear Wheel Speed PGN ", VIPData.STAT_REAR_WHEEL_SPEED.J1939.attributes.PGN);
+                    print("Battery PGN ", VIPData.STAT_VBATTERY.J1939.attributes.PGN);
+                    print("Fule Level PGN ", VIPData.STAT_FUEL_LEVEL.J1939.attributes.PGN);
+                    print("Odometer PGN ", VIPData.STAT_NVM_ODOMETER.J1939.attributes.PGN);
+                    print("Gear Indicator PGN ", VIPData.STAT_GEAR_INDICATOR.J1939.attributes.PGN);
+                else :
+                    print("JSON Parse Error")
+                jsonfile.close();
+            else :
+                print("File Open Failed");
+        else :
+            print("No File ", path," Exists");
+    else :
+        print("Input File Extention is not JSON");
 else :
-	print("No File ", path," Exists");    
+    print("Invalid arguments");
+    print("Help : \n Syntax : python validateJSON.py <jsonFileName> "); 
+    
